79917701ebfda4cdc864aaf01e7c779f91bde30d
Lasse Lang <lang.lasse@gmx.de>
2024-11-24 14:46:50 +0100

    Added displaying of other bond types and added valence electrons

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let outputString = ""
 const distance = 50;
 const xLineOffset = 8;
 const yLineOffset = -7;
+const offset = 4;
 
 let autoDraw = true;
 
@@ -41,7 +42,7 @@ ctx.fillText("C", width/2, height/2);
 
 let firstNode = document.getElementsByClassName("node")[0];
 firstNode.setAttribute("nodeID", "0");
-firstNode.firstElementChild.onchange = updateType;
+firstNode.firstElementChild.oninput = updateType;
 addAddButton(firstNode);
 nodeIDs = 1
 
@@ -55,10 +56,11 @@ function addNode(event) {
   addTypeInput(node);
   addBondTypeSelect(node);
   addAngleInput(node);
+  addValenceElectronInput(node);
   addRemoveButton(node);
   addAddButton(node);
   node.setAttribute("nodeID", nodeIDs);
-  newNode = new chemNode(nodeIDs, "C", "Single", 0, []);
+  newNode = new chemNode(nodeIDs, "C", "Single", 0, [], [,,,]);
   nodes[event.target.parentNode.getAttribute("nodeID")].children.push(newNode);
   event.target.parentNode.insertBefore(node, event.target);
   nodes[nodeIDs] = newNode;
@@ -115,7 +117,7 @@ function addTypeInput(parent) {
   typeInput.type = "text";
   typeInput.placeholder = "Type";
   typeInput.value = "C";
-  typeInput.onchange = updateType;
+  typeInput.oninput = updateType;
   parent.appendChild(typeInput);
 }
 
@@ -124,7 +126,7 @@ function addAngleInput(parent) {
   angleInput.type = "number";
   angleInput.placeholder = "Angle";
   angleInput.value = 0;
-  angleInput.onchange = updateAngle;
+  angleInput.oninput = updateAngle;
   parent.appendChild(angleInput);
 }
 
@@ -143,8 +145,22 @@ function addBondTypeSelect(parent) {
   option.value = "Trible";
   option.innerText = "Trible";
   select.appendChild(option);
-  select.onchange = updateBond;
+  select.oninput = updateBond;
   parent.appendChild(select);
+}
+
+function addValenceElectronInput(parent) {
+  let div = document.createElement("div");
+  div.classList.add("valenceElectronsContainer");
+  for (i = 0; i<4; i++) {
+    input = document.createElement("input");
+    input.type = "number";
+    input.classList.add("valenceElectronsInput");
+    input.oninput = updateValenceElectron;
+    input.setAttribute("index", i);
+    div.appendChild(input);
+  }
+  parent.appendChild(div);
 }
 
 function create() {
@@ -154,8 +170,26 @@ function create() {
 
 function generateCode() {
   outputString = "\\chemfig{";
-
-  outputString += rootNode.type;
+  
+  a = true;
+  rootNode.valenceElectrons.forEach(valenceElectron => {
+    if (valenceElectron != "" && valenceElectron != undefined) {
+      if (a) {
+        outputString += "\\charge{"
+        a = false
+      }
+      else {
+        outputString += ","
+      }
+      outputString += `${valenceElectron}=\\|`;
+    }
+  })
+  if (a) {
+    outputString += rootNode.type;
+  }
+  else {
+    outputString += `}{${rootNode.type}}`;
+  }
 
   codeChildren(rootNode.children);
 
@@ -167,7 +201,9 @@ function generateCode() {
 
 function codeChildren(children) {
   children.forEach(child => {
-    outputString += "("
+    if (children.length > 1) {
+      outputString += "("
+    }
     switch (child.bond) {
       case "Single":
         outputString += "-";
@@ -179,9 +215,32 @@ function codeChildren(children) {
         outputString += "~";
         break;
     }
-    outputString += `[:${child.angle}]${child.type}`
+    if (child.angle != 0) {
+      outputString += `[:${child.angle}]`
+    }
+    a = true;
+    child.valenceElectrons.forEach(valenceElectron => {
+      if (valenceElectron != "" && valenceElectron != undefined) {
+        if (a) {
+          outputString += "\\charge{"
+          a = false
+        }
+        else {
+          outputString += ","
+        }
+        outputString += `${valenceElectron}=\\|`;
+      }
+    })
+    if (a) {
+      outputString += child.type;
+    }
+    else {
+      outputString += `}{${child.type}}`;
+    }
     codeChildren(child.children)
-    outputString += ")";
+    if (children.length > 1) {
+      outputString += ")";
+    }
   })
 }
 
@@ -190,36 +249,167 @@ function draw() {
   let yPosition = height/2;
   ctx.clearRect(0, 0, width, height);
   ctx.fillText(rootNode.type, xPosition, yPosition);
-  drawRecursive(rootNode.children, rootNode, xPosition, yPosition);
+
+  rootNode.valenceElectrons.forEach(valenceElectron => {
+    if (valenceElectron != "") {
+      degree = (valenceElectron-40)
+      degree2 = (parseInt(valenceElectron)+40)
+      ctx.beginPath();
+      ctx.moveTo(
+        xPosition+xLineOffset + Math.round(Math.cos(degree * (Math.PI / 180))*(xLineOffset+6), 1),
+        yPosition+yLineOffset - Math.round(-Math.sin(degree * (Math.PI / 180))*(yLineOffset-6), 1)
+      );
+      ctx.lineTo(
+        xPosition+xLineOffset + Math.round(Math.cos(degree2 * (Math.PI / 180))*(xLineOffset+6), 1),
+        yPosition+yLineOffset - Math.round(-Math.sin(degree2 * (Math.PI / 180))*(yLineOffset-6), 1)
+      );
+      ctx.closePath();
+      ctx.stroke();
+    }
+  })
+
+  drawRecursive(rootNode.children, rootNode.type, xPosition, yPosition);
 }
 
-function drawRecursive(nodes, oldNode, xPosition, yPosition) {
+function drawRecursive(nodes, oldNodeType, xPosition, yPosition) {
   nodes.forEach(node => {
     newY = Math.round(-Math.sin(node.angle * (Math.PI / 180))*distance, 1);
     newX = Math.round(Math.cos(node.angle * (Math.PI / 180))*distance, 1);
     ctx.fillText(node.type, xPosition+newX, yPosition+newY);
 
-    ctx.beginPath();
-    if (node.type != "" && oldNode.type != "") {
-      ctx.moveTo(xPosition+xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1), yPosition+yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1));
-      ctx.lineTo(xPosition+newX+xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1), yPosition+newY+yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4)));
-    }
-    else if (node.type == "" && oldNode.type != "") {
-      ctx.moveTo(xPosition+xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1), yPosition+yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1));
-      ctx.lineTo(xPosition+newX+xLineOffset, yPosition+newY+yLineOffset);
-    }
-    else if (node.type != "" && oldNode.type == "") {
-      ctx.moveTo(xPosition+xLineOffset, yPosition+yLineOffset);
-      ctx.lineTo(xPosition+newX+xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1), yPosition+newY+yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4)));
-    }
-    else if (node.type == "" && oldNode.type == "") {
-      ctx.moveTo(xPosition+xLineOffset, yPosition+yLineOffset);
-      ctx.lineTo(xPosition+newX+xLineOffset, yPosition+newY+yLineOffset);
-    }
-    ctx.closePath();
-    ctx.stroke();
+    node.valenceElectrons.forEach(valenceElectron => {
+      if (valenceElectron != "") {
+        degree = (valenceElectron-40)
+        degree2 = (parseInt(valenceElectron)+40)
+        ctx.beginPath();
+        ctx.moveTo(
+          xPosition+xLineOffset+newX + Math.round(Math.cos(degree * (Math.PI / 180))*(xLineOffset+6), 1),
+          yPosition+yLineOffset+newY - Math.round(-Math.sin(degree * (Math.PI / 180))*(yLineOffset-6), 1)
+        );
+        ctx.lineTo(
+          xPosition+xLineOffset+newX + Math.round(Math.cos(degree2 * (Math.PI / 180))*(xLineOffset+6), 1),
+          yPosition+yLineOffset+newY - Math.round(-Math.sin(degree2 * (Math.PI / 180))*(yLineOffset-6), 1)
+        );
+        ctx.closePath();
+        ctx.stroke();
+      }
+    });
 
-    drawRecursive(node.children, node, xPosition+newX, yPosition+newY);
+    if (node.bond == "Single") {
+      ctx.beginPath();
+      if (node.type != "" && oldNodeType != "") {
+        ctx.moveTo(
+          xPosition+xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1),
+          yPosition+yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1)
+        );
+        ctx.lineTo(
+          xPosition+newX+xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1),
+          yPosition+newY+yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1)
+        );
+      }
+      else if (node.type == "" && oldNodeType != "") {
+        ctx.moveTo(
+          xPosition+xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1), 
+          yPosition+yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1)
+        );
+        ctx.lineTo(
+          xPosition+newX+xLineOffset, 
+          yPosition+newY+yLineOffset
+        );
+      }
+      else if (node.type != "" && oldNodeType == "") {
+        ctx.moveTo(
+          xPosition+xLineOffset, 
+          yPosition+yLineOffset
+        );
+        ctx.lineTo(
+          xPosition+newX+xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1),
+          yPosition+newY+yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1)
+        );
+      }
+      else if (node.type == "" && oldNodeType == "") {
+        ctx.moveTo(
+          xPosition+xLineOffset, 
+          yPosition+yLineOffset
+        );
+        ctx.lineTo(
+          xPosition+newX+xLineOffset, 
+          yPosition+newY+yLineOffset
+        );
+      }
+      ctx.closePath();
+      ctx.stroke();
+    }
+    else if (node.bond == "Double") {
+      perpendicularOffsetX = Math.round(-Math.sin(node.angle * (Math.PI / 180)) * offset, 1);
+      perpendicularOffsetY = Math.round(Math.cos(node.angle * (Math.PI / 180)) * offset, 1);
+
+      ctx.beginPath();
+      ctx.moveTo(
+          xPosition + xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) + perpendicularOffsetX,
+          yPosition + yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) + perpendicularOffsetY
+      );
+      ctx.lineTo(
+          xPosition + newX + xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) + perpendicularOffsetX,
+          yPosition + newY + yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) + perpendicularOffsetY
+      );
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(
+          xPosition + xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) - perpendicularOffsetX,
+          yPosition + yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) - perpendicularOffsetY
+      );
+      ctx.lineTo(
+          xPosition + newX + xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) - perpendicularOffsetX,
+          yPosition + newY + yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) - perpendicularOffsetY
+      );
+      ctx.closePath();
+      ctx.stroke();
+    }
+    else if (node.bond == "Trible") {
+      perpendicularOffsetX = Math.round(-Math.sin(node.angle * (Math.PI / 180)) * offset, 1);
+      perpendicularOffsetY = Math.round(Math.cos(node.angle * (Math.PI / 180)) * offset, 1);
+
+      ctx.beginPath();
+      ctx.moveTo(
+          xPosition + xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) + perpendicularOffsetX,
+          yPosition + yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) + perpendicularOffsetY
+      );
+      ctx.lineTo(
+          xPosition + newX + xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) + perpendicularOffsetX,
+          yPosition + newY + yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) + perpendicularOffsetY
+      );
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(
+          xPosition + xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) - perpendicularOffsetX,
+          yPosition + yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) - perpendicularOffsetY
+      );
+      ctx.lineTo(
+          xPosition + newX + xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180)) * (xLineOffset + 4), 1) - perpendicularOffsetX,
+          yPosition + newY + yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180)) * (yLineOffset - 4), 1) - perpendicularOffsetY
+      );
+      ctx.closePath();
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(
+        xPosition+xLineOffset + Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1),
+        yPosition+yLineOffset - Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4), 1)
+      );
+      ctx.lineTo(
+        xPosition+newX+xLineOffset - Math.round(Math.cos(node.angle * (Math.PI / 180))*(xLineOffset+4), 1),
+        yPosition+newY+yLineOffset + Math.round(-Math.sin(node.angle * (Math.PI / 180))*(yLineOffset-4))
+      );
+      ctx.closePath();
+      ctx.stroke();
+
+    }
+
+    drawRecursive(node.children, node.type, xPosition+newX, yPosition+newY);
   })
 }
 
@@ -244,16 +434,24 @@ function updateAngle(event) {
   }
 }
 
+function updateValenceElectron(event) {
+  nodes[event.target.parentNode.parentNode.getAttribute("nodeID")].valenceElectrons[event.target.getAttribute("index")] = event.target.value;
+  if (autoDraw) {
+    draw()
+  }
+}
+
 class chemNode {
-  constructor(id, type, bond, angle, children) {
+  constructor(id, type, bond, angle, children, valenceElectrons) {
     this.id = id;
     this.type = type;
     this.bond = bond;
     this.angle = angle;
     this.children = children;
+    this.valenceElectrons = valenceElectrons;
   }
 }
 
-let rootNode = new chemNode(0, "C", "", 0, [])
+let rootNode = new chemNode(0, "C", "", 0, [], [,,,])
 
 nodes = {"0": rootNode}
